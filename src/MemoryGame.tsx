755473886e5870e234cee3fe6377267da8b41dde
Lexa Huang <sixian.duidui@gmail.com>
2024-06-23 17:45:14 +0800

    rename button event handler

--- src/MemoryGame.tsx
+++ src/MemoryGame.tsx
@@ -10,7 +10,7 @@ type ImageObject = {
 };
 
 type MemoryGameProps = {
-    testImages: string[];
+  testImages: string[];
 };
 
 function createImageArray(images: string[]): ImageObject[] {
@@ -23,9 +23,9 @@ function createImageArray(images: string[]): ImageObject[] {
   return imageArray;
 }
 
-function checkWin(imageArray: ImageObject[]): boolean{
-    const isWin = imageArray.every(image => image.flipped);
-    return isWin;
+function checkWin(imageArray: ImageObject[]): boolean {
+  const isWin = imageArray.every((image) => image.flipped);
+  return isWin;
 }
 
 const MemoryGame = ({ testImages }: MemoryGameProps) => {
@@ -34,51 +34,49 @@ const MemoryGame = ({ testImages }: MemoryGameProps) => {
   const [prevImage, setPrevImage] = useState<ImageObject | null>();
   const [isWin, setIsWin] = useState<boolean>(false);
 
-  function handleImageClick( image: ImageObject ): void {
+  function handleButtonClick(image: ImageObject): void {
     const newImageArray = imageArray.slice();
 
     //flip this image
     newImageArray[image.id].flipped = true;
 
     //check whether this image matches the previous image if there is a previous
-    if (prevImage){
-        if (image.img === prevImage.img){
-            newImageArray[prevImage.id].flipped = true;
-            setPrevImage(null);
-        } else {
-            newImageArray[prevImage.id].flipped = false;
-            setPrevImage(image)
-        }
+    if (prevImage) {
+      if (image.img === prevImage.img) {
+        newImageArray[prevImage.id].flipped = true;
+        setPrevImage(null);
+      } else {
+        newImageArray[prevImage.id].flipped = false;
+        setPrevImage(image);
+      }
     } else {
-        setPrevImage(image)
+      setPrevImage(image);
     }
 
     //re-render the whole image grid
     setImageArray(newImageArray);
 
-    if (checkWin(newImageArray)){
-        setIsWin(true);
+    if (checkWin(newImageArray)) {
+      setIsWin(true);
     }
   }
 
   return (
     <>
-    <h1>Memory Game</h1>
-    <div className="grid-container">
-      {imageArray.map(({ id, img, flipped }) =>
-        flipped ? (
-          <img src={img} className="square image"></img>
-        ) : (
-          <button 
-                  className="square button" 
-                  onClick={() => handleImageClick({ id, img, flipped })}>
-          </button>
-        )
-      )}
-    </div>
-    {isWin && (
-        <h2> Congratulations!</h2>
-    )}
+      <h1>Memory Game</h1>
+      <div className="grid-container">
+        {imageArray.map(({ id, img, flipped }) =>
+          flipped ? (
+            <img src={img} className="square image"></img>
+          ) : (
+            <button
+              className="square button"
+              onClick={() => handleButtonClick({ id, img, flipped })}
+            ></button>
+          )
+        )}
+      </div>
+      {isWin && <h2> Congratulations!</h2>}
     </>
   );
 };
